3cfe0447e59694bf0adb9f9372d381bd068f97de
Danilo Rajcevic <danilorajcevic14@gmail.com>
2023-09-26 18:31:50 -0300

    Copia app coder con mejora de templates

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,9 +1,14 @@
-from django.contrib import admin
+
 from django.urls import path
-from .views import(
-    saludo,
-    muestra_nombre,
-    probando_template,
-    usando_loader,
-    curso
-)
+
+from AppCoder import views
+
+urlpatterns = [
+    
+    path("", views.inicio), #Esta es nuestra 1er view
+    path("cursos", views.cursos, name="Cursos"),
+    path("profesores", views.profesores),
+    path("estudiantes", views.estudiantes),
+    path("entregables", views.entregables),
+    
+]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -5,23 +5,42 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+# def inicio(request):
+    
+#     return HttpResponse("vista inicio")
+
+# def cursos(request):
+    
+#     return HttpResponse("vista cursos")
+
+# def profesores(request):
+    
+#     return HttpResponse("Vista profesores")
+
+# def estudiantes(request):
+    
+#     return HttpResponse("Vista estudiantes")
+
+# def entregables(request):
+    
+#     return HttpResponse("vista entregables")    ESTO YA NO VA PORQUE LO REEMPLAZAMOS POR EL RENDER DE LA PLANTILLA
+
 def inicio(request):
     
-    return HttpResponse("vista inicio")
+    return render(request, "appCoder/inicio.html")
 
 def cursos(request):
     
-    return HttpResponse("vista cursos")
+    return render(request, "appCoder/cursos.html")
 
 def profesores(request):
     
-    return HttpResponse("Vista profesores")
+    return render(request, "appCoder/profesores.html")
 
 def estudiantes(request):
     
-    return HttpResponse("Vista estudiantes")
+    return render(request, "appCoder/estudiantes.html")
 
 def entregables(request):
-    
-    return HttpResponse("vista entregables")
 
+    return render(request, "appCoder/entregables.html")
