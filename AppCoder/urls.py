591eacecd76872bb6d4a9ad93f7747a441cf6737
Danilo Rajcevic <danilorajcevic14@gmail.com>
2023-09-27 21:10:41 -0300

    Todo lo ultimo que hice de herencia

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -5,7 +5,7 @@ from AppCoder import views
 
 urlpatterns = [
     
-    path("", views.inicio), #Esta es nuestra 1er view
+    path("", views.inicio, name="Inicio"), #Esta es nuestra 1er view
     path("cursos", views.cursos, name="Cursos"),
     path("profesores", views.profesores, name="Profesores"),
     path("estudiantes", views.estudiantes, name="Estudiantes"),
